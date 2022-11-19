7eb7e046be9b8f0a9d2d001eee2e7b65205ab39e
Данил <bigwitch3r@gmail.com>
2022-11-19 20:53:21 +0300

    Добавьте файлы проекта.

---
+++ Employees/DBM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Employees
+{
+    internal class DBM
+    {
+        private SQLiteConnection cnn;
+        public void connect()
+        {
+            cnn = new SQLiteConnection("Data Source=Employees.sqlite3");
+            cnn.Open();
+        }
+
+        public void disconnect()
+        {
+            cnn.Close();
+        }
+
+        public void create_table()
+        {
+            SQLiteCommand command = cnn.CreateCommand();
+            command.CommandText = "create table if not exists Persons (id integer primary key, lastname text not null, firstname text not null, middlename text not null, birthdate text not null, worksfrom text not null, gender text not null);";
+            command.ExecuteNonQuery();
+        }
+
+        public void data_fill(DataGridView dataGridView1)
+        {
+            SQLiteDataAdapter da = new SQLiteDataAdapter("select id, lastname, firstname, middlename,gender," +
+                "birthdate, worksfrom from Persons", cnn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
